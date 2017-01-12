b3389302566e51199815979614df7d243186c396
cvmufeed <cvmufeed@gmail.com>
2017-01-12 17:28:10 +0530

    added a timer

--- app/src/main/java/com/mufeed/alarm/AlarmReceiver.java
+++ app/src/main/java/com/mufeed/alarm/AlarmReceiver.java
@@ -15,15 +15,15 @@ public class AlarmReceiver extends BroadcastReceiver
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
-        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(new long[]{0, 500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40, 500}, -1);
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null)
-        {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        }
-        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-        ringtone.play();
+            Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+            Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(new long[]{0, 500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40, 500}, -1);
+
+            Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            if (alarmUri == null) {
+                alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            }
+            Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+            ringtone.play();
     }
 }
--- app/src/main/java/com/mufeed/alarm/MainActivity.java
+++ app/src/main/java/com/mufeed/alarm/MainActivity.java
@@ -4,19 +4,31 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     TimePicker alarmTimePicker;
     PendingIntent pendingIntent;
     AlarmManager alarmManager;
+    TextView timerValue;
+    Button timerStart,timerStop,timerReset;
+    private long startTime = 0L;
+    private Handler customHandler = new Handler();
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +37,14 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        timerValue = (TextView) findViewById(R.id.timer_value);
+        timerStart = (Button) findViewById(R.id.timer_start);
+        timerStop = (Button) findViewById(R.id.timer_stop);
+        timerReset = (Button) findViewById(R.id.timer_reset);
+        timerStart.setOnClickListener(this);
+        timerReset.setOnClickListener(this);
+        timerStop.setOnClickListener(this);
+
     }
     public void OnToggleClicked(View view)
     {
@@ -38,6 +58,7 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, AlarmReceiver.class);
             pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
             time=(calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
+            //if the alarm is less than current time ... set for tomorrow if 24hr... or next 12 hrs if 12hr
             if(System.currentTimeMillis()>time)
             {
                 if (calendar.AM_PM == 0)
@@ -45,7 +66,7 @@ public class MainActivity extends AppCompatActivity
                 else
                     time = time + (1000*60*60*24);
             }
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 60000, pendingIntent);
         }
         else
         {
@@ -53,4 +74,42 @@ public class MainActivity extends AppCompatActivity
             Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == timerStart){
+            startTime = SystemClock.uptimeMillis();
+            customHandler.postDelayed(updateTimerThread, 0);
+        }
+        else if (view == timerStop) {
+            timeSwapBuff += timeInMilliseconds;
+            customHandler.removeCallbacks(updateTimerThread);
+            timerStart.setText(R.string.resume);
+        }
+        else if (view == timerReset) {
+            customHandler.removeCallbacks(updateTimerThread);
+            startTime = 0L;
+            timerValue.setText(getString(R.string._00_00_00));
+            timeInMilliseconds = 0L;
+            timeSwapBuff = 0L;
+            updatedTime = 0L;
+            timerStart.setText(getString(R.string.start));
+        }
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            timerValue.setText("" + mins + ":"+ String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+    };
+
 }
